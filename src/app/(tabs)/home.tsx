4f0ac2f81305c621ca217d76211299d6b00323f0
MANITRELA <manitrelarakotomalala@gmail.com>
2026-09-11 16:13:50 +0300

    Premier commit

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,19 +1,58 @@
-import { router } from "expo-router";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { auth } from "@/config/firebase";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
+
 
 export default function Home(){
+
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect (() => {
+        // Ecouter etat du connexion
+
+        const unsubcribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        })
+
+        return unsubcribe;
+    },[]);
+
+    if(loading){
+        return(
+            <View style = {style.loading}>
+                <ActivityIndicator size="large" color="#2563EB"/>
+                <Text>Chargement...</Text>
+            </View>
+        )
+    }
+
+    if (!user){
+        return(
+            <View style = {style.container}>
+                <Text>Aucun utilisateur connecté</Text>
+            </View>
+        )
+    }
+
     return(
         <View style = {style.container}>
-            <Pressable onPress={() => {router.replace("/")} }>
-                <Text>Deconnecter</Text>
-            </Pressable>
-            <Text style = {style.title}>
-                Bienvenue sur MYDOCTOR
-            </Text>
+            {/* Header */}
+            <View style = {style.header}>
+                <View>
+                    <Text style = {style.smallText}>Bonjour {user.displayName || "Utilisateurs"}</Text>
+                    <Text style = {style.title}>MYDOCTOR  {user.email}</Text>
+                </View>
 
-            <Text style = {style.text}>
-                Accueil
-            </Text>
+                {user.photoURL?(<Image source={{uri: user.photoURL}} style = {style.profileImage}/>):(
+                    <Text>Pas de photo</Text>
+                )}
+
+            </View>
+
+
         </View>
     )
 }
@@ -32,10 +71,51 @@ const style = StyleSheet.create({
         color: "#444"
     },
 
+    header : {
+        flexDirection : "row",
+        justifyContent : "space-between",
+        alignItems: "center",
+        marginTop : 40,
+        marginBottom: 30
+    },
+
+    smallText :{
+        fontSize : 16,
+        color : "#777"
+    },
+
+    profileImage : {
+        width : 65,
+        height : 65,
+        borderRadius : 35
+    },
+
+    defaultAvatar :{
+        width : 65,
+        height : 65,
+        borderRadius : 35,
+        backgroundColor : "#2563EB",
+        justifyContent : "center",
+        alignItems: "center"
+    },
+
+    avatarText : {
+        fontSize: 28,
+        color: "white",
+        fontWeight: "bold"
+    },
+
     text : {
         fontSize : 16,
         color: "#888",
         marginTop: 10
+    },
+
+    loading: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 20
     }
     
 })
--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,14 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey:"",
-    authDomain:"",
-    projectId:"",
-    storageBucket:"",
-    messagingSenderId:"",
-    appId:""
+  apiKey: "AIzaSyBBuuUFocZ5ZWTsmbDNL24D_q_De78AwHM",
+  authDomain: "mydoctor-d5382.firebaseapp.com",
+  projectId: "mydoctor-d5382",
+  storageBucket: "mydoctor-d5382.firebasestorage.app",
+  messagingSenderId: "290233203809",
+  appId: "1:290233203809:web:aa2e472918069c628dfe20",
+  measurementId: "G-H3K0C8KTW4"
 };
+
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
